16a1dd49f059501238919318f345a603c3d9c92d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-09-19 10:11:11 -0700

    refactor: weatherCard weatherConditions moved to utils folder

--- src/WeatherCard/WeatherCard.js
+++ src/WeatherCard/WeatherCard.js
@@ -1,35 +1,4 @@
-const weatherConditions = [
-  {
-    weather: "sunny",
-    url: require("../images/day-weather/sunny.svg").default,
-    day: true,
-  },
-  {
-    weather: "cloudy",
-    url: require("../images/day-weather/cloudy.svg").default,
-    day: true,
-  },
-  {
-    weather: "rain",
-    url: require("../images/day-weather/rain.svg").default,
-    day: true,
-  },
-  {
-    weather: "storm",
-    url: require("../images/day-weather/storm.svg").default,
-    day: true,
-  },
-  {
-    weather: "snow",
-    url: require("../images/day-weather/snow.svg").default,
-    day: true,
-  },
-  {
-    weather: "fog",
-    url: require("../images/day-weather/fog.svg").default,
-    day: true,
-  },
-];
+import { weatherConditions } from "../utils/utils";
 
 const WeatherCard = ({ day = true, weather = "sunny", weatherTemp = 0 }) => {
   const imageSrc = weatherConditions.filter((i) => {
