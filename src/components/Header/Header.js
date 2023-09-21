ac9e4fc88a282fc86a0c3272d03ba5fecb4cba40
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-09-20 20:10:13 -0700

    update: file structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = ({onCreateModal}) => {
   return (
     <header className="header">
       <div className="header__logo-icon">
-        <img src={require('../images/logo.svg').default} />
+        <img src={require('../../images/logo.svg').default} />
         <div>{currentDate}</div>
       </div>
       <div className="header__avatar-icon">
@@ -14,7 +14,7 @@ const Header = ({onCreateModal}) => {
           <button type="text" onClick={onCreateModal} >+ Add clothes</button>
         </div>
         <div>Terrence Tegegne</div>
-        <img src={require("../images/avatar.svg").default} alt="avatar logo" />
+        <img src={require("../../images/avatar.svg").default} alt="avatar logo" />
       </div>
     </header>
   );
