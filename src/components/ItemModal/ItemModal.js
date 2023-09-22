97b0886a45e4d608fdfefebdba55635c5b5de9ca
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-09-21 21:21:45 -0700

    styles: itemModal

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -2,16 +2,18 @@ import "./ItemModal.css";
 
 const ItemModal = ({ selectedCard, onClose }) => {
   return (
-    <div className="modal">
-      <div className="modal__content modal_content-item">
+    <div className="modal__preview">
+      <div className="modal__content">
         <button className="modal__close" onClick={onClose}></button>
         <img
           className="modal__image"
           src={selectedCard.link}
           alt={`picture of a ${selectedCard.name}`}
         />
-        <div>{selectedCard.name}</div>
-        <div>Weather: {selectedCard.weather}</div>
+        <div className="modal__description-wrap">
+          <div className="modal__description">{selectedCard.name}</div>
+          <div className="modal__description">Weather: {selectedCard.weather}</div>
+        </div>
       </div>
     </div>
   );
