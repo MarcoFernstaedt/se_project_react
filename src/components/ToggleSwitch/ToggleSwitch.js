50c97221ac0bbbfc2028a380fff532669259a680
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-10-12 20:01:54 -0700

    styles: add className

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react';
-import './ToggleSwitch.css'
-import { CurrentTemperatureUnitContext } from '../../contexts/CurrentTemperatureUnitContext';
+import React, { useContext } from "react";
+import "./ToggleSwitch.css";
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 const ToggleSwitch = () => {
-    const {currentTemperatureUnit, handleToggleSwitchChange} = useContext(CurrentTemperatureUnitContext);
+  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
+    CurrentTemperatureUnitContext
+  );
 
-    return (
-        <label className='switch' for='switch'>
-            <input id='switch' type='checkbox' className='switch__box' onChange={handleToggleSwitchChange} />
-            <span className='slider slider_left'>{currentTemperatureUnit}</span>
-        </label>
-    )
-}
+  return (
+    <label className="switch" for="switch">
+      <input
+        id="switch"
+        type="checkbox"
+        className="switch__checkbox"
+        onChange={handleToggleSwitchChange}
+      />
+      <p className="switch__temp-unit-f">F</p>
+      <p className="switch__temp-unit-c">C</p>
+      <span
+        className='switch__slider'>
+      </span>
+    </label>
+  );
+};
 
 export default ToggleSwitch;
