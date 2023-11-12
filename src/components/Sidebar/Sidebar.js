a0eabab0cab1c83c3f1c31874d938018c4193a44
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-11-11 22:25:21 -0700

    style: Format Sidebar.js using Prettier for improved readability and removed unnecessary link import

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,15 @@
-import avatarImage from '../../images/avatar.svg'
-import { Link } from "react-router-dom";
-import './Sidebar.css';
+import avatarImage from "../../images/avatar.svg";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-    return (
-        <>
+  return (
+    <>
       <div className="sidebar__avatar-image">
         <img src={avatarImage} alt="profile picture" />
       </div>
-      <div className='sidebar__profile-info'>
-            Terrence Tegegne
-      </div>
+      <div className="sidebar__profile-info">Terrence Tegegne</div>
     </>
-    )
-}
+  );
+};
 
 export default Sidebar;
