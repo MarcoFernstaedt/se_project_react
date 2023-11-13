6d84ca9e15db385dc11c83c849250390923d57c9
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-11-12 22:02:12 -0700

    refactor: Replace fetch calls with reusable 'request' function in API calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,9 @@
 const baseUrl = "http://localhost:3001";
 
+const request = (url, options) => {
+  return fetch(url, options).then(processServerResponce);
+};
+
 const processServerResponce = (res) => {
   if (!res.ok) {
     return Promise.reject(`Error: ${res.status}`);
@@ -8,27 +12,31 @@ const processServerResponce = (res) => {
 };
 
 const getCards = () => {
-  return fetch(`${baseUrl}/items`).then(processServerResponce);
+  return request(`${baseUrl}/items`);
 };
 
-const postCard = ({name, imageUrl, weather}) => {
-  return fetch(`${baseUrl}/items`, {
-    method: 'POST',
+const postCard = ({ name, imageUrl, weather }) => {
+  const options = {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name: name,
       imageUrl: imageUrl,
       weather: weather,
-    })
-  }).then(processServerResponce)
-}
+    }),
+  };
+
+  return request(`${baseUrl}/items`, options);
+};
 
 const deleteCard = (id) => {
-  return fetch(`${baseUrl}/items/${id}`, {
-    method: 'DELETE',
-  }).then(processServerResponce);
+  const deleteOpiions = {
+    method: "DELETE",
+  };
+
+  return request(`${baseUrl}/items/${id}`, deleteOpiions);
 };
 
 export { getCards, postCard, deleteCard };
