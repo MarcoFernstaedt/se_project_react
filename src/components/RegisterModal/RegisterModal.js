d1c9ea038fecda1478b741665095f863cf63b391
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-26 16:21:17 -0700

    create registerModal

---
+++ src/components/RegisterModal/RegisterModal.js
@@ -0,0 +1,34 @@
+import { useForm } from "../../hooks/useForm";
+import ModalWithForm from '../ModalWithForm';
+
+const RegisterModal = ({ isOpen, onClose, onSubmit }) => {
+    const {values, handleChange, setValues} = useForm({});
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const [name, email, password, avatar] = values
+      onSubmit({ name, email, password, avatar });
+    };
+  
+    return (
+      <ModalWithForm
+        title="Register"
+        onClose={onClose}
+        // modalName="login"
+        isOpen={isOpen}
+        onSubmit={handleSubmit}
+      >
+        <label className="modal__label">Name</label>
+        <input className="modal__input modal__input_type_text" type="text" value={values.name} onChange={handleChange} placeholder="Name" />
+        <label className="modal__label">Email</label>
+        <input className="modal__input modal__input_type_text" type="email" value={values.email} onChange={handleChange} placeholder="Email" />
+        <label className="modal__label">Password</label>
+        <input className="modal__input modal__input_type_text" type="password" value={values.password} onChange={handleChange} placeholder="Password" />
+        <label className="modal__label">name</label>
+        <input className="modal__input modal__input_type_text" type="url" value={values.AvatarUrl} onChange={handleChange} placeholder="Avatar Url" />
+        <button type="submit">Register</button>
+      </ModalWithForm>
+    );
+  };
+  
+  export { RegisterModal };
