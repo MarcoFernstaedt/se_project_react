f010a2affdbe0e3fb924effb2214cb95dd35ef6f
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-03-27 14:02:45 -0700

    fix exports

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -1,30 +1,42 @@
 import { useForm } from "../../hooks/useForm";
-import ModalWithForm from '../ModalWithForm';
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 const LoginModal = ({ isOpen, onClose, onSubmit }) => {
-    const {values, handleChange, setValues} = useForm({});
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const [email, password] = values
-      onSubmit({ email, password });
-    };
-  
-    return (
-      <ModalWithForm
-        title="Login"
-        onClose={onClose}
-        // modalName="login"
-        isOpen={isOpen}
-        onSubmit={handleSubmit}
-      >
-        <label className="modal__label">Email</label>
-        <input className="modal__input modal__input_type_text" type="email" value={values.email} onChange={handleChange} placeholder="Email" />
-        <label className="modal__label">Password</label>
-        <input className="modal__input modal__input_type_text" type="password" value={values.password} onChange={handleChange} placeholder="Password" />
-        <button type="submit">Login</button>
-      </ModalWithForm>
-    );
+  const { values, handleChange, setValues } = useForm({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const [email, password] = values;
+    onSubmit({ email, password });
   };
-  
-  export { LoginModal };
+
+  return (
+    <ModalWithForm
+      title="Login"
+      onClose={onClose}
+      // modalName="login"
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+    >
+      <label className="modal__label">Email</label>
+      <input
+        className="modal__input modal__input_type_text"
+        type="email"
+        value={values.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <label className="modal__label">Password</label>
+      <input
+        className="modal__input modal__input_type_text"
+        type="password"
+        value={values.password}
+        onChange={handleChange}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </ModalWithForm>
+  );
+};
+
+export default LoginModal;
