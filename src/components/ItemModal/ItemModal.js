dcdfd608ba1c9a35ecd4d7b8e3f24c46f308ae70
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-04-10 14:03:21 -0700

    implament logic for delete button visability

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,9 +1,20 @@
 import { useEscape } from "../../hooks/useEscape";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import "./ItemModal.css";
+import { useContext } from "react";
 
 const ItemModal = ({ selectedCard, onClose, openModal, buttonText }) => {
-  // use userContext 
+  const { currentUser, isLoggedIn } = useContext(CurrentUserContext);
+  // use userContext
   useEscape(onClose);
+
+  // Checking if the current user is the owner of the current clothing item
+  const isOwn = selectedCard.owner === currentUser._id;
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const itemDeleteButtonClassName = `modal__delete-button ${
+    isOwn ? "modal__delete-button_visible" : "modal__delete-button_hidden"
+  }`;
 
   return (
     <div className="modal modal-preview">
@@ -23,7 +34,7 @@ const ItemModal = ({ selectedCard, onClose, openModal, buttonText }) => {
             <button
               onClick={openModal}
               type="button"
-              className="modal__delete-button"
+              className={itemDeleteButtonClassName}
             >
               {buttonText}
             </button>
