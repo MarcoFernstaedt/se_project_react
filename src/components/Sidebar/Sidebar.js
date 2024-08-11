9272764330c684c2f38c59e75e665d0e5ff6a375
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-08-10 22:31:01 -0700

    wip: logout button in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,20 +1,20 @@
- import { useContext } from "react";
+import { useContext } from "react";
 // import avatarImage from "../../images/avatar.svg";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import "./Sidebar.css";
 
 const Sidebar = (handleOpenEditProfileModal, handleLogOut) => {
-  const [currentUser, isLoggedIn] = useContext(CurrentUserContext);
+  const [currentUser] = useContext(CurrentUserContext);
 
   return (
     <>
       <div
         onClick={handleOpenEditProfileModal}
         className="sidebar__avatar-image"
-      >
+       >
         <img src={currentUser.avatar} alt="Your avatar" />
       </div>
-      <div className="sidebar__profile-info">currentUser.name</div>
+      <div className="sidebar__profile-info" >currentUser.name</div>
     </>
   );
 };
