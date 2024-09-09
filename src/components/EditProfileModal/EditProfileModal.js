047c13893c925ab268309b9eb636e002dc68736d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-09-09 16:25:18 -0700

    implament submit button

--- src/components/EditProfileModal/EditProfileModal.js
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -4,30 +4,44 @@ import { useForm } from "../../hooks/useForm";
 
 const EditProfileModal = ({ buttonText, onClose, isOpen, onSubmit }) => {
   const { values, handleChange, setValues } = useForm({});
+  
+  const handleSubmit = (e) => {
+    console.log(`modal values - name: ${values.name} avatar: ${values.avatar}`)
+    e.preventDefault();
+    onSubmit(values);
+  };
+  
   return (
     <ModalWithForm
       title="Edit Profile"
       onClose={onClose}
       isOpen={isOpen}
-      onSubmit={onSubmit}
+      onSubmit={handleSubmit}
       buttonText={buttonText}
     >
-      <label className="modal__labal">Name</label>
+      <label className="modal__label">Name</label>
       <input
         className="modal__input modal__input_type_text"
         type="text"
+        name='name'
         value={values.name || ""}
         onChange={handleChange}
         placeholder="Name"
       />
-      <label className="modal__labal">Avatar</label>
+      <label className="modal__label">Avatar</label>
       <input
         className="modal__input modal__input_type_text"
         type="url"
+        name='avatar'
         value={values.avatar || ""}
         onChange={handleChange}
         placeholder="Avatar"
       />
+      <div className="modal__button-container">
+        <button className="modal__submit-button" type="submit">
+          {buttonText}
+        </button>
+      </div>
     </ModalWithForm>
   );
 };
