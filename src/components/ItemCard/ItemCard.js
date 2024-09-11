6e296b140f4177a1766cdd8a139a7ad410dcf5cc
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-09-11 16:56:48 -0700

    like button visable when user is logged in

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -3,26 +3,32 @@ import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import "./ItemCard.css";
 
 const ItemCard = ({ item, onSelectCard, onCardLike }) => {
-  const { currentUser } = useContext(CurrentUserContext)
+  const { currentUser } = useContext(CurrentUserContext);
   // Check if the item was liked by the current user
   // The likes array should be an array of ids
-  const isLiked = item.likes.some(id => id === currentUser._id);
-  
+  // const isLiked = currentUser ? item.likes.some(id => id === currentUser._id);
+  const isLiked = item.likes.some((id) => id === currentUser._id);
+
   // Create a variable which you then set in `className` for the like button
-  const itemLikeButtonClassName = isLiked ? 'itemCard__button-like_active' : 'itemCard__button-like_inactive'
+  const itemLikeButtonClassName = `itemCard__button-like ${
+    isLiked ? "itemCard__button-like_active" : "itemCard__button-like_inactive"
+  }`;
 
   const handleLikes = () => {
-    let id = item.owner
+    let id = item.owner;
     onCardLike({ id, isLiked });
   };
+
   return (
     <li className="itemCard">
       <div className="itemCard__header">
         <h3 className="itemCard__name">{item.name}</h3>
-        <button
-          className={`itemCard__button-like ${itemLikeButtonClassName}`}
-          onClick={handleLikes}
-        ></button>
+        {currentUser && (
+          <button
+            className={itemLikeButtonClassName}
+            onClick={handleLikes}
+          ></button>
+        )}
       </div>
       <img
         src={item.imageUrl}
