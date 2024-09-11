bc011aa74671f653215268de607a156550bc578b
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-09-11 15:45:26 -0700

    handle card likes param passed through

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,12 +1,28 @@
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import "./ItemCard.css";
 
-const ItemCard = ({ item, onSelectCard }) => {
-  console.log(item)
+const ItemCard = ({ item, onSelectCard, onCardLike }) => {
+  const { currentUser } = useContext(CurrentUserContext)
+  // Check if the item was liked by the current user
+  // The likes array should be an array of ids
+  const isLiked = item.likes.some(id => id === currentUser._id);
+  
+  // Create a variable which you then set in `className` for the like button
+  const itemLikeButtonClassName = isLiked ? 'itemCard__button-like_active' : 'itemCard__button-like_inactive'
+
+  const handleLikes = () => {
+    let id = item.owner
+    onCardLike({ id, isLiked });
+  };
   return (
     <li className="itemCard">
       <div className="itemCard__header">
         <h3 className="itemCard__name">{item.name}</h3>
-        <button className='itemCard__button-like'></button>
+        <button
+          className={`itemCard__button-like ${itemLikeButtonClassName}`}
+          onClick={handleLikes}
+        ></button>
       </div>
       <img
         src={item.imageUrl}
