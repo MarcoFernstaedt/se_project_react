e492d61111c49bd984e23b54a1cc2cc5ac50cd64
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-09-11 12:04:13 -0700

    pass currentTemperatureUnit to getWeatherType

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ import "./Main.css";
 
 const Main = ({ weatherTemp, onSelectCard, clothingItems }) => {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
-  const weather = getWeatherType(weatherTemp);
+  const weather = getWeatherType({weatherTemp, currentTemperatureUnit});
   const filteredCards = clothingItems.filter((item) => {
     return item.weather && item.weather.toLowerCase() === weather;
   });
